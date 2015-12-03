0b273fff915e0366ec83035ac85b2a28bc776804
mcnuggets-lab <mcnuggets23@gmail.com>
2015-12-03 22:33:46 +0800

    message

--- wsgi/djangodemo/djangodemo/settings.py
+++ wsgi/djangodemo/djangodemo/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'djangodemo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(WSGI_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
