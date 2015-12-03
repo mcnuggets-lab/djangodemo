d0746148300bcd462655143eaaf7c91b70d4750c
mcnuggets-lab <mcnuggets23@gmail.com>
2015-12-03 21:53:32 +0800

    commit

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     # GETTING-STARTED: set your app name:
-    name='YourAppName',
+    name='djangodemoapp',
     # GETTING-STARTED: set your app version:
     version='1.0',
     # GETTING-STARTED: set your app description:
     description='OpenShift App',
     # GETTING-STARTED: set author name (your name):
-    author='Your Name',
+    author='McNuggets-Lab',
     # GETTING-STARTED: set author email (your email):
     author_email='example@example.com',
     # GETTING-STARTED: set author url (your url):
